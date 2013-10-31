96b3d9cf4e4ccac99304a99cdeeb6ae6da558a25
Abhijit Joshi <jabhiji@abhijits-mbp.gateway.2wire.net>
2013-10-30 23:29:42 -0400

    Added the GPU version to KONSOL

--- KONSOL/gpu_lbm.cu
+++ KONSOL/gpu_lbm.cu
@@ -9,18 +9,18 @@ Last modified on: Thursday, July 18 2013 @12:08 pm
 
 Build instructions: make (uses Makefile present in this folder)
 
-Run instructions: ./cpu_lbm
+Run instructions: optirun ./gpu_lbm
 
 */
 
-#include<iostream>     // cout
-#include<cmath>        // pow
+#include<iostream>
+#include<stdio.h>
 #include<ctime>        // clock_t, clock(), CLOCKS_PER_SEC
 
 // problem parameters
 
 const int     N = 128;                  // number of node points along X and Y (cavity length in lattice units)
-const int     TIME_STEPS = 1000000;     // number of time steps for which the simulation is run
+const int     TIME_STEPS = 10000;       // number of time steps for which the simulation is run
 const double  REYNOLDS_NUMBER = 1E6;    // REYNOLDS_NUMBER = LID_VELOCITY * N / kinematicViscosity
 
 // don't change these unless you know what you are doing
@@ -31,87 +31,98 @@ const double  LID_VELOCITY = 0.05;      // lid velocity in lattice units
 
 // D2Q9 parameters 
 
+__constant__ double ex[Q];
+__constant__ double ey[Q];
+__constant__ int oppos[Q];
+__constant__ double wt[Q];
+
 // populate D3Q19 parameters and copy them to __constant__ memory on the GPU
 
-void D3Q9(double *ex, double *ey, int *oppos, double *wt)
+void D3Q9(double *ex_h, double *ey_h, int *oppos_h, double *wt_h)
 {
     // D2Q9 model base velocities and weights
 
-    ex[0] =  0.0;   ey[0] =  0.0;   wt[0] = 4.0 /  9.0;
-    ex[1] =  1.0;   ey[1] =  0.0;   wt[1] = 1.0 /  9.0;
-    ex[2] =  0.0;   ey[2] =  1.0;   wt[2] = 1.0 /  9.0;
-    ex[3] = -1.0;   ey[3] =  0.0;   wt[3] = 1.0 /  9.0;
-    ex[4] =  0.0;   ey[4] = -1.0;   wt[4] = 1.0 /  9.0;
-    ex[5] =  1.0;   ey[5] =  1.0;   wt[5] = 1.0 / 36.0;
-    ex[6] = -1.0;   ey[6] =  1.0;   wt[6] = 1.0 / 36.0;
-    ex[7] = -1.0;   ey[7] = -1.0;   wt[7] = 1.0 / 36.0;
-    ex[8] =  1.0;   ey[8] = -1.0;   wt[8] = 1.0 / 36.0;
+    ex_h[0] =  0.0;   ey_h[0] =  0.0;   wt_h[0] = 4.0 /  9.0;
+    ex_h[1] =  1.0;   ey_h[1] =  0.0;   wt_h[1] = 1.0 /  9.0;
+    ex_h[2] =  0.0;   ey_h[2] =  1.0;   wt_h[2] = 1.0 /  9.0;
+    ex_h[3] = -1.0;   ey_h[3] =  0.0;   wt_h[3] = 1.0 /  9.0;
+    ex_h[4] =  0.0;   ey_h[4] = -1.0;   wt_h[4] = 1.0 /  9.0;
+    ex_h[5] =  1.0;   ey_h[5] =  1.0;   wt_h[5] = 1.0 / 36.0;
+    ex_h[6] = -1.0;   ey_h[6] =  1.0;   wt_h[6] = 1.0 / 36.0;
+    ex_h[7] = -1.0;   ey_h[7] = -1.0;   wt_h[7] = 1.0 / 36.0;
+    ex_h[8] =  1.0;   ey_h[8] = -1.0;   wt_h[8] = 1.0 / 36.0;
 
     // define opposite (anti) aections (useful for implementing bounce back)
 
-    oppos[0] = 0;      //      6        2        5
-    oppos[1] = 3;      //               ^
-    oppos[2] = 4;      //               |
-    oppos[3] = 1;      //               |
-    oppos[4] = 2;      //      3 <----- 0 -----> 1
-    oppos[5] = 7;      //               |
-    oppos[6] = 8;      //               |
-    oppos[7] = 5;      //               v
-    oppos[8] = 6;      //      7        4        8
+    oppos_h[0] = 0;      //      6        2        5
+    oppos_h[1] = 3;      //               ^
+    oppos_h[2] = 4;      //               |
+    oppos_h[3] = 1;      //               |
+    oppos_h[4] = 2;      //      3 <----- 0 -----> 1
+    oppos_h[5] = 7;      //               |
+    oppos_h[6] = 8;      //               |
+    oppos_h[7] = 5;      //               v
+    oppos_h[8] = 6;      //      7        4        8
+
+    // copy to constant (read-only) memory
+    cudaMemcpyToSymbol(ex,    ex_h,    Q * sizeof(double));  // x-component of velocity direction
+    cudaMemcpyToSymbol(ey,    ey_h,    Q * sizeof(double));  // y-component of velocity direction
+    cudaMemcpyToSymbol(oppos, oppos_h, Q * sizeof(int));     // opposite direction for each velocity direction
+    cudaMemcpyToSymbol(wt,    wt_h,    Q * sizeof(double));  // weight factor for velocity direction
 }
 
 // initialize values for aection vectors, density, velocity and distribution functions on the GPU
 
-void initialize(const int N, const int Q, const double DENSITY, const double LID_VELOCITY, 
-                double *ex, double *ey, int *oppos, double *wt,
-                double *rho, double *ux, double *uy, double* sigma, 
-                double *f, double *feq, double *f_new)
+__global__ void initialize(const int N, const int Q, const double DENSITY, const double LID_VELOCITY, 
+                           double *rho, double *ux, double *uy, double* sigma, 
+                           double *f, double *feq, double *f_new)
 {
-    // loop over all voxels
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
+    // compute the global "i" and "j" location handled by this thread
 
-            // natural index for location (i,j)
+    const int i = blockIdx.x * blockDim.x + threadIdx.x ;
+    const int j = blockIdx.y * blockDim.y + threadIdx.y ;
 
-            int index = i*N+j;  // column-ordering
+    // bound checking
+    if( (i > (N-1)) || (j > (N-1)) ) return;
 
-            // initialize density and velocity fields inside the cavity
+    // natural index for location (i,j)
 
-              rho[index] = DENSITY;   // density
-               ux[index] = 0.0;       // x-component of velocity
-               uy[index] = 0.0;       // x-component of velocity
-            sigma[index] = 0.0;       // rate-of-strain field
+    const int index = i*N+j;  // column-ordering
 
-            // specify boundary condition for the moving lid
+    // initialize density and velocity fields inside the cavity
 
-            if(j==0) ux[index] = LID_VELOCITY;
+      rho[index] = DENSITY;   // density
+       ux[index] = 0.0;       // x-component of velocity
+       uy[index] = 0.0;       // x-component of velocity
+    sigma[index] = 0.0;       // rate-of-strain field
 
-            // assign initial values for distribution functions
-            // along various aections using equilibriu, functions
+    // specify boundary condition for the moving lid
 
-            for(int a=0;a<Q;a++) {
-        
-                int index_f = a + index*Q;
+    if(j==0) ux[index] = LID_VELOCITY;
 
-                double edotu = ex[a]*ux[index] + ey[a]*uy[index];
-                double udotu = ux[index]*ux[index] + uy[index]*uy[index];
+    // assign initial values for distribution functions
+    // along various aections using equilibriu, functions
 
-                feq[index_f]   = rho[index] * wt[a] * (1.0 + 3.0*edotu + 4.5*edotu*edotu - 1.5*udotu);
-                f[index_f]     = feq[index_f];
-                f_new[index_f] = feq[index_f];
+    #pragma unroll
+    for(int a=0;a<Q;a++) {
 
-            }
+        int index_f = a + index*Q;
 
-        }
+        double edotu = ex[a]*ux[index] + ey[a]*uy[index];
+        double udotu = ux[index]*ux[index] + uy[index]*uy[index];
+
+        feq[index_f]   = rho[index] * wt[a] * (1.0 + 3.0*edotu + 4.5*edotu*edotu - 1.5*udotu);
+        f[index_f]     = feq[index_f];
+        f_new[index_f] = feq[index_f];
+
     }
 }
 
 // this function updates the values of the distribution functions at all points along all directions
 // carries out one lattice time-step (streaming + collision) in the algorithm
 
-void collideAndStream(// READ-ONLY parameters (used by this function but not changed)
+__global__ void collideAndStream(// READ-ONLY parameters (used by this function but not changed)
                                  const int N, const int Q, const double DENSITY, const double LID_VELOCITY, const double REYNOLDS_NUMBER,
-                                 const double *ex, const double *ey, const int *oppos, const double *wt,
                                  // READ + WRITE parameters (get updated in this function)
                                  double *rho,         // density
                                  double *ux,         // X-velocity
@@ -121,12 +132,16 @@ void collideAndStream(// READ-ONLY parameters (used by this function but not cha
                                  double *feq,        // equilibrium distribution function
                                  double *f_new)      // new distribution function
 {
-    // loop over all interior voxels
-    for(int i = 1; i < N-1; i++) {
-        for(int j = 1; j < N-1; j++) {
+    // compute the global "i" and "j" location handled by this thread
+
+    const int i = blockIdx.x * blockDim.x + threadIdx.x ;
+    const int j = blockIdx.y * blockDim.y + threadIdx.y ;
+
+    // bound checking
+    if( (i < 1) || (i > (N-2)) || (j < 1) || (j > (N-2)) ) return;
 
     // natural index
-    int index = i*N + j;  // column-major ordering
+    const int index = i*N + j;  // column-major ordering
 
     // calculate fluid viscosity based on the Reynolds number
     double kinematicViscosity = LID_VELOCITY * (double) N / REYNOLDS_NUMBER;
@@ -135,6 +150,7 @@ void collideAndStream(// READ-ONLY parameters (used by this function but not cha
     double tau =  0.5 + 3.0 * kinematicViscosity;
 
     // collision
+    #pragma unroll
     for(int a=0;a<Q;a++) {
         int index_f = a + index*Q;
         double edotu = ex[a]*ux[index] + ey[a]*uy[index];
@@ -144,6 +160,7 @@ void collideAndStream(// READ-ONLY parameters (used by this function but not cha
 
     // streaming from interior node points
 
+    #pragma unroll
     for(int a=0;a<Q;a++) {
 
         int index_f = a + index*Q;
@@ -187,14 +204,10 @@ void collideAndStream(// READ-ONLY parameters (used by this function but not cha
             f_new[index_nbr_f] = f_plus;
         }
     }
-
-        } // j
-    }//i
 }
 
-void everythingElse( // READ-ONLY parameters (used by this function but not changed)
+__global__ void everythingElse( // READ-ONLY parameters (used by this function but not changed)
                                 const int N, const int Q, const double DENSITY, const double LID_VELOCITY, const double REYNOLDS_NUMBER,
-                                 const double *ex, const double *ey, const int *oppos, const double *wt,
                                 // READ + WRITE parameters (get updated in this function)
                                 double *rho,         // density
                                 double *ux,         // X-velocity
@@ -204,14 +217,19 @@ void everythingElse( // READ-ONLY parameters (used by this function but not chan
                                 double *feq,        // equilibrium distribution function
                                 double *f_new)      // new distribution function
 {
-    // loop over all interior voxels
-    for(int i = 1; i < N-1; i++) {
-        for(int j = 1; j < N-1; j++) {
+    // compute the global "i" and "j" location of this thread
+
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    const int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+    // bound checking
+    if( (i < 1) || (i > (N-2)) || (j < 1) || (j > (N-2)) ) return;
 
     // natural index
-    int index = i*N + j;  // column-major ordering
+    const int index = i*N + j;  // column-major ordering
 
     // push f_new into f
+    #pragma unroll
     for(int a=0;a<Q;a++) {
         int index_f = a + index*Q;
         f[index_f] = f_new[index_f];
@@ -260,60 +278,73 @@ void everythingElse( // READ-ONLY parameters (used by this function but not chan
                  + pow(sum_zx,2) + pow(sum_zy,2) + pow(sum_zz,2);
 
     sigma[index] = pow(sigma[index],0.5);
-
-        }//j
-    }//i
 }
 
 int main(int argc, char* argv[])
 {
-        // allocate memory
+        // allocate memory on the GPU
 
         // distribution functions
-        double *f = new double[N*N*Q];
-        double *feq = new double[N*N*Q];
-        double *f_new = new double[N*N*Q];
+        double *f, *feq, *f_new;
+        cudaMalloc((void **)&f,N*N*Q*sizeof(double));
+        cudaMalloc((void **)&feq,N*N*Q*sizeof(double));
+        cudaMalloc((void **)&f_new,N*N*Q*sizeof(double));
 
         // density and velocity
-        double *rho = new double[N*N];
-        double *ux = new double[N*N];
-        double *uy = new double[N*N];
+        double *rho, *ux, *uy;
+        cudaMalloc((void **)&rho,N*N*sizeof(double));
+        cudaMalloc((void **)&ux,N*N*sizeof(double));
+        cudaMalloc((void **)&uy,N*N*sizeof(double));
 
         // rate-of-strain
-        double *sigma = new double[N*N];
+        double *sigma;
+        cudaMalloc((void **)&sigma,N*N*sizeof(double));
 
-        // D3Q9 parameters
-        double *ex = new double[Q];
-        double *ey = new double[Q];
-        int *oppos = new int[Q];
-        double *wt = new double[Q];
+        // allocate space for D3Q9 parameters on the host
+        double *ex_h    = new double[Q];
+        double *ey_h    = new double[Q];
+        int *oppos_h = new int[Q];
+        double *wt_h = new double[Q];
 
         // fill D3Q9 parameters in constant memory on the GPU
-        D3Q9(ex, ey, oppos, wt);
+        D3Q9(ex_h, ey_h, oppos_h, wt_h);
+
+        // assign a 2D distribution of CUDA "threads" within each CUDA "block"    
+        int threadsAlongX=16, threadsAlongY=16;
+        dim3 dimBlock(threadsAlongX, threadsAlongY, 1);
+
+        // calculate number of blocks along X and Y in a 2D CUDA "grid"
+        dim3 dimGrid( ceil(float(N)/float(dimBlock.x)), ceil(float(N)/float(dimBlock.y)), 1 );
 
         // launch GPU kernel to initialize all fields
-        initialize(N, Q, DENSITY, LID_VELOCITY, ex, ey, oppos, wt, rho, ux, uy, sigma, f, feq, f_new);
+        initialize<<<dimGrid,dimBlock>>>(N, Q, DENSITY, LID_VELOCITY,
+                                         rho, ux, uy, sigma,
+                                         f, feq, f_new);
 
         // time integration
         int time=0;
         while(time<TIME_STEPS) {
 
-            time++;     // lattice time
+            time++;
 
             clock_t t;
 	    t = clock();
 
-            collideAndStream(N, Q, DENSITY, LID_VELOCITY, REYNOLDS_NUMBER, ex, ey, oppos, wt, rho, ux, uy, sigma, f, feq, f_new);
+            collideAndStream<<<dimGrid,dimBlock >>>(N, Q, DENSITY, LID_VELOCITY, REYNOLDS_NUMBER,
+                                                    rho, ux, uy, sigma,
+                                                    f, feq, f_new);
 
             // collideAndStream and everythingElse were originally one kernel
             // they were separated out to make all threads synchronize globally
             // before moving on to the next set of calculations
 
-            everythingElse(N, Q, DENSITY, LID_VELOCITY, REYNOLDS_NUMBER, ex, ey, oppos, wt, rho, ux, uy, sigma, f, feq, f_new);
-
+            everythingElse<<<dimGrid,dimBlock >>>(N, Q, DENSITY, LID_VELOCITY, REYNOLDS_NUMBER,
+                                                  rho, ux, uy, sigma,
+                                                  f, feq, f_new);
             t = clock() - t;
 	    std::cout << "Lattice time " << time << " clock ticks " << t << " lattice time steps per second = " << (float) CLOCKS_PER_SEC / (float) t << std::endl;
+
         }
-    
+
     return 0;
 }
